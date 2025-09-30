bc4a211077a1a0982c9057e696913b29e2061ec0
JanielleOliveira <janielleoliver.brito@gmail.com>
2025-09-29 23:58:53 -0300

    Implementar CategoriaService e CategoriaController com método findAll()

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
 import { Categoria } from './categoria/entities/categoria.entity';
-import { CategoriaModule } from './categoria/entities/categoria.module';
 
 @Module({
   imports: [
